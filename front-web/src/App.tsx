cbabf5eb57c24d59d9fd381d7f8ad16d29b033bc
unknown <ps.fernanda29@gmail.com>
2021-01-06 16:04:10 -0300

    Add home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import Home from './Home';
 import Navbar from './Navbar';
 
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div>
       <Navbar />
+      <Home />
     </div>
   );
 }
