5890925b35d117995c20570229428816a42d4641
unknown <ps.fernanda29@gmail.com>
2021-01-08 23:12:42 -0300

    Error correction

--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -14,7 +14,7 @@ function Home() {
                             Faça seu pedido <br /> que entregamos <br /> pra você!!!
                     </h1>
                         <h3 className="home-subtitle">
-                            Escolha o seu pedido e em poucos minutos <br /> levaremoss na sua porta
+                            Escolha o seu pedido e em poucos minutos <br /> levaremos na sua porta
                     </h3>
                         <Link to="/orders" className="home-btn-order">
                             FAZER PEDIDO
