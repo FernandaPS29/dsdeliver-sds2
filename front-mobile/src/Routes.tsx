b18b4229c2810a91c0e45adee1730374816f86ea
unknown <ps.fernanda29@gmail.com>
2021-01-10 14:24:38 -0300

    Front-mobile ok

--- front-mobile/src/Routes.tsx
+++ front-mobile/src/Routes.tsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from "./Home";
 import Orders from "./Orders";
+import OrderDetails from "./OrderDetails";
 const Stack = createStackNavigator();
 
 function Routes () {
@@ -17,6 +18,7 @@ function Routes () {
             }}>
                 <Stack.Screen name="Home" component={Home}></Stack.Screen>
                 <Stack.Screen name="Orders" component={Orders}></Stack.Screen>
+                <Stack.Screen name="OrderDetails" component={OrderDetails}></Stack.Screen>
             </Stack.Navigator>
         </NavigationContainer>
     )
